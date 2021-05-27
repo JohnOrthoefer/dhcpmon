f761c5b1cc083f03cfafc2b51fd214db31406e78
John Orthoefer <john@orthoefer.org>
2021-05-27 00:41:29 -0400

    working

--- main.go
+++ main.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-   "log"
    "fmt"
-   "io/ioutil"
 )
 
 const leasesFile="dnsmasq.leases"
 
-func check(e error, s string) {
-   if e != nil {
-      log.Fatalf("%s %v", s, e)
-   }
-}
-
 func main() {
-   leases, err := ioutil.ReadFile(leasesFile)
-   check(err, leasesFile)
 
-   d := ParseLeases(string(leases))
+   readLeases(leasesFile)
+   go watchLeases(leasesFile)
 
-   fmt.Printf("Leases %d\n", len(d))
+   fmt.Printf("Leases %d\n", len(GetLeases()))
 
    fmt.Printf("Starting HTTP\n")
    startHTTP()
--- parseLeases.go
+++ parseLeases.go
@@ -2,15 +2,17 @@ package main
 
 import (
    "log"
-   "fmt"
    "strings"
    "strconv"
    "time"
    "net"
+   "os"
+   "github.com/fsnotify/fsnotify"
 )
 
 type DHCPEntry struct {
    Expire time.Time
+   Remain time.Duration
    MAC net.HardwareAddr
    IP net.IP
    Name string
@@ -19,11 +21,65 @@ type DHCPEntry struct {
 
 var dhcpLeases []DHCPEntry
 
+// check(err, leasesFile)
+func check(e error, s string) {
+   if e != nil {
+      log.Fatalf("%s %v", s, e)
+   }
+}
+
 func GetLeases() []DHCPEntry {
+   for i, ent := range dhcpLeases {
+      dhcpLeases[i].Remain = time.Until(ent.Expire).Truncate(time.Second)
+   }
    return dhcpLeases
 }
 
+func readLeases(f string) {
+   leases, err := os.ReadFile(f)
+   check(err, f)
+
+   ParseLeases(string(leases))
+}
+
+func watchLeases(f string) {
+   watcher, err := fsnotify.NewWatcher()
+   if err != nil {
+      log.Fatal(err)
+   }
+   defer watcher.Close()
+
+   done := make(chan bool)
+   go func() {
+      for {
+         select {
+         case event, ok := <-watcher.Events:
+            if !ok {
+               return
+            }
+            log.Println("event:", event)
+            if event.Op&fsnotify.Write == fsnotify.Write {
+               log.Println("modiied file", event.Name)
+               readLeases(f)
+            }
+         case err, ok := <-watcher.Errors:
+            if !ok {
+               return
+            }
+            log.Println("error:", err)
+         }
+      }
+   }()
+
+   err = watcher.Add(f)
+   if err != nil {
+      log.Fatal(err)
+   }
+   <-done
+}
+
 func ParseLeases(l string) []DHCPEntry {
+   dhcpLeases = dhcpLeases[:0]
 
    for i,line := range strings.Split(l, "\n") {
       c := strings.Split(line, " ")
@@ -35,31 +91,32 @@ func ParseLeases(l string) []DHCPEntry {
          switch j {
          // Expire Time
          case 0:
-           v,_ := strconv.ParseInt(ent, 10, 64)
-           e.Expire = time.Unix(v,0)
-           fmt.Printf("%v ", time.Until(e.Expire).Truncate(time.Second).String())
+            v,_ := strconv.ParseInt(ent, 10, 64)
+            e.Expire = time.Unix(v,0)
+            e.Remain = time.Until(e.Expire)
+//            fmt.Printf("%v ", time.Until(e.Expire).Truncate(time.Second).String())
          // MAC Address
          case 1:
-           v,_ := net.ParseMAC(ent)
-           e.MAC = v
-           fmt.Printf("%v ", v)
+            v,_ := net.ParseMAC(ent)
+            e.MAC = v
+//            fmt.Printf("%v ", v)
          // Assigned IP
          case 2:
-           v := net.ParseIP(ent)
-           e.IP = v
-           fmt.Printf("%v ", v)
+            v := net.ParseIP(ent)
+            e.IP = v
+//            fmt.Printf("%v ", v)
          // Name
          case 3:
-           v := ent
-           e.Name = v
-           fmt.Printf("%v ", v)
+            v := ent
+            e.Name = v
+//            fmt.Printf("%v ", v)
          // DHCP Identifer If supplied
          case 4:
-           v := ent
-           e.ID = v
-           fmt.Printf("%v\n", v)
+            v := ent
+            e.ID = v
+//            fmt.Printf("%v\n", v)
          default:
-           log.Printf("Parse Out of range Line:%d Field:%d\n", i, j)
+            log.Printf("Parse Out of range Line:%d Field:%d\n", i, j)
          }
       }
       dhcpLeases = append(dhcpLeases, e)
